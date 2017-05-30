0e0351116a775fa5cdaadd276899fd57a2a0c250
h1-the-swan <jason.portenoy@gmail.com>
2017-05-30 09:09:56 -0700

    checkbox for tooltips

--- visualization_star/groups/vis.js
+++ visualization_star/groups/vis.js
@@ -215,28 +215,56 @@ function nodeTooltips() {
 		// content: "error",
 		contentAsHTML: true,
 		functionInit: function(instance, helper) {
-			var tooltipHtml = getTooltipHtml(helper.origin);
+			// var tooltipHtml = getTooltipHtml(helper.origin);
 			// instance.content($(helper.origin).data("tooltipHtml"));
 			// d3.select(helper.origin).each(function(d) {instance.content(d.author_name_detex)});
-			instance.content(tooltipHtml);
-		}
+			// instance.content(tooltipHtml);
+			$( '#tooltipCheckbox' ).change( function() { this.checked ? instance.enable() : instance.disable(); } );
+			instance.content('error');
+		},
+		functionFormat: tooltipFormat
 	});
+	// tooltipster-discovery mode so tooltips open quicker in succession
+	// https://github.com/louisameline/tooltipster-discovery
+	$.tooltipster.group('.node circle');
 
 
-	function getTooltipHtml(node) {
-		// store tooltip html as data attribute
-		var html;
-		d3.select(node).each(function(d) {
-			var span = $( '<span>' );
-			span.append( $('<p class="tooltip author_name">').text(d.author_name_detex) );
-			span.append( $('<p class="tooltip affil_name">').text(d.affil_name) );
+	function tooltipFormat(instance, helper, content) {
+		var thisNode = d3.select(helper.origin);
+		var $template = $( '#tooltip-template' ).clone().show();
+		function fillHtml(classname, textContent) {
+			$template.find( '.' + classname ).find( '.template-content' ).text(textContent);
+		}
+
+		thisNode.each(function(d) {
+			fillHtml('author_name', d.author_name_detex);
+			fillHtml('affil_name', d.affil_name);
+			fillHtml('cluster_id', d.cl_bottom);
+			var $paperTitles = $template.find( '.paper_titles' ).find( '.template-content' );
 			for (var i = 0, len = d.papers.length; i < len; i++) {
-				span.append( $( '<p class="tooltip paper_title">' ).text(d.papers[i].title) );
+				$listItem = $( '<li class="paper_title">' ).text(d.papers[i].title);
+				$paperTitles.append( $listItem );
 			}
-			html = span.html();
 		});
-		return html;
+		// return $template.html();
+		return $template;
 	}
+
+
+	// function getTooltipHtml(node) {
+	// 	// store tooltip html as data attribute
+	// 	var html;
+	// 	d3.select(node).each(function(d) {
+	// 		var span = $( '<span>' );
+	// 		span.append( $('<p class="tooltip author_name">').text(d.author_name_detex) );
+	// 		span.append( $('<p class="tooltip affil_name">').text(d.affil_name) );
+	// 		for (var i = 0, len = d.papers.length; i < len; i++) {
+	// 			span.append( $( '<p class="tooltip paper_title">' ).text(d.papers[i].title) );
+	// 		}
+	// 		html = span.html();
+	// 	});
+	// 	return html;
+	// }
 
 	
 }
@@ -253,4 +281,5 @@ function applyRadioSelection() {
 $( document ).ready(function() {
 	var $nodelabRadio = $( 'input[type=radio][name=nodelab-radio]' );
 	$nodelabRadio.change( applyRadioSelection );
+	
 })
